0d8da949f39ccc6fe135a43f4d9b6a7a654037cf
tokusan1015 <tomsan_1015-develop@yahoo.co.jp>
2020-08-21 08:17:34 +0900

    プロジェクト ファイルを追加します。

---
+++ TemperatureForecast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperatureForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
